f5a4222e66ac46f9eb68ab244d5ef441bc37a61f
oliahrytsak <oliagrytsak1991@gmail.com>
2023-06-20 12:49:04 +0300

    css

--- index.js
+++ index.js
@@ -165,3 +165,34 @@ checkboxes.forEach((checkbox) => {
     saveTasks();
   });
 });
+
+
+
+
+//Привітання
+const currentTime = new Date();
+ const timeString = currentTime.toLocaleTimeString('uk');
+ const greeting = document.querySelector(".greeting");
+ const newParagraph = document.createElement("p");
+
+ switch (true) {
+case (timeString >= '23:00:00' || timeString < '05:00:00'):
+   
+    newParagraph.textContent = "Good night!";
+  greeting .appendChild(newParagraph);
+    break;
+  case (timeString >= '05:00:00' && timeString < '11:00:00'):
+    
+    newParagraph.textContent = "Good morning!";
+  greeting .appendChild(newParagraph);
+     break;
+  case (timeString >= '11:00:00' && timeString < '17:00:00'):
+    newParagraph.textContent = "Good afternoon!";
+  greeting .appendChild(newParagraph);
+    break;
+  case (timeString > '17:00:00' && timeString < '23:00:00'):
+    newParagraph.textContent = "Good evening!";
+  greeting .appendChild(newParagraph);
+}
+
+console.log(timeString);
